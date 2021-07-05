8463589b08599b60d93c5c62c7b7f5f946191a6c
Elsa Lebas <lebas.elsa@gmail.com>
2021-07-05 22:17:24 +0200

    setup for front

---
+++ app.js
@@ -0,0 +1,22 @@
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+import { searchBox, hits } from 'instantsearch.js/es/widgets';
+
+const searchClient = algoliasearch('F3K65Y2QCC', '1a8775809faeaf09ef5607fc788842bb');
+
+const search = instantsearch({
+  indexName: 'sing-sing',
+  searchClient,
+});
+
+search.addWidgets([
+  searchBox({
+    container: "#searchbox"
+  }),
+
+  hits({
+    container: "#hits"
+  })
+]);
+
+search.start();
